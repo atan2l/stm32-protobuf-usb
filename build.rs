c3fdd2446b10f495d03dbbee898eb6284b517c23
Carlos Amores <cadenaskarlos@gmail.com>
2025-05-12 03:22:15 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() {
     femtopb_build::compile_protos(&["proto/command.proto", "proto/response.proto"], &["proto"])
         .unwrap();
+
+    println!("cargo:rustc-link-arg-bins=--nmagic");
+    println!("cargo:rustc-link-arg-bins=-Tlink.x");
+    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
